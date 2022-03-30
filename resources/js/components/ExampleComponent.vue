4e97f19fd9664fbf7abb8e338954db0b444dd6f2
Prewcio <Prewcioyt@gmail.com>
2022-03-30 22:51:52 +0200

    Fixed redirect

--- resources/js/components/ExampleComponent.vue
+++ resources/js/components/ExampleComponent.vue
@@ -2,14 +2,19 @@
   <div class="container">
     <div class="home">
       <h1>Darmowy, ciekawy i skuteczny sposób na naukę języka!</h1>
-      <button>Rozpocznij</button>
-      <button>Mam juz konto</button>
+      <button @click="redirect('register')">Rozpocznij</button>
+      <button @click="redirect('login')">Mam juz konto</button>
     </div>
   </div>
 </template>
 
 <script>
 export default {
+  methods:{
+    redirect: function (params) {
+        window.location.href = "/"+params;
+    }
+  },
   mounted() {
     console.log("Component mounted.");
   },
